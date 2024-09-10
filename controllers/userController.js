ac236e052774f5869cfdfd49258e3b7f86b9e725
zubairmaqsood <149279927+zubairmaqsood@users.noreply.github.com>
2024-09-10 22:21:02 +0500

    update create user api and delete user api and also create update api of user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,48 +3,59 @@ const bcrypt = require("bcrypt")
 const generateToken = require("../utils/generateToken")
 const organizationModel = require("../models/organizationModel")
 
-module.exports.createUser =  async (req, res) => {
+module.exports.createUser = async (req, res) => {
     try {
         const { name, email, password, phone, gender, idCardNo, role, createdAt, updatedAt } = req.body
-        const organizationId = req.params.id
+        const organizationId = req.params.orgId
         const user = await userModel.findOne({ email })
         if (user) return res.status(409).json({ message: "User already exist" })
 
         const salt = await bcrypt.genSalt(12)
         const hash = await bcrypt.hash(password, salt)
 
-        const createdUser = await userModel.create({ name, email,password: hash,gender,idCardNo,phone,role,organization: organizationId,createdAt,updatedAt })
+        const createdUser = await userModel.create({ name, email, password: hash, gender, idCardNo, phone, role, organization: organizationId, createdAt, updatedAt })
 
-        const organization = await organizationModel.findOne({_id:organizationId})
-        if(!organization) return res.status(404).json({message:"Organization Not Found"})
+        const organization = await organizationModel.findOne({ _id: organizationId })
+        if (!organization) return res.status(404).json({ message: "Organization Not Found" })
 
         organization.users.push(createdUser._id)
         await organization.save()
-        
+
         const token = generateToken(createdUser)
-        res.cookie("token",token)
-        res.status(201).json({message:"User created successfully"})
+        res.cookie("token", token)
+        res.status(201).json({ message: "User created successfully" })
     } catch (err) {
-        res.status(500).json({message:err.message})
+        res.status(500).json({ message: err.message })
     }
 }
 
-module.exports.deleteUser = async(req,res)=>{
-    try{
-    const id = req.params.id
-    const user = await userModel.findOne({_id:id})
-    if(!user) return res.status(404).send({message:"User Not Found"})
+module.exports.deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id
+        const user = await userModel.findOne({ _id: id })
+        if (!user) return res.status(404).send({ message: "User Not Found" })
 
-    const organizationId = user.organization.toString()
-    const organization = await organizationModel.findOne({_id:organizationId})
-    if(!organization) return res.status(404).json({message:"Organization Not Found"})
+        const organizationId = user.organization.toString()
+        const organization = await organizationModel.findOne({ _id: organizationId })
+        if (!organization) return res.status(404).json({ message: "Organization Not Found" })
 
-    const index = organization.users.indexOf(id)
-    organization.users.splice(index,1)
-    await organization.save()
-    await userModel.deleteOne({_id:id})
-    res.status(200).json({message:"User delted Successfully"})
-    }catch(error){
-        res.status(500).json({message:error.message})
+        const index = organization.users.indexOf(id)
+        organization.users.splice(index, 1)
+        await organization.save()
+        await userModel.deleteOne({ _id: id })
+        res.status(200).json({ message: "User delted Successfully" })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+module.exports.updateUser = async(req, res) => {
+    try {
+        const id = req.params.id 
+        const user = await userModel.findByIdAndUpdate(id, { ...req.body }, { runValidators: true, new: true })
+        if(user==null) return res.status(404).json({message:"User Not Found"})
+        res.status(200).json({message:"User updated successfully"})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const router = express.Router()
-const {createUser,deleteUser} = require("../controllers/userController")
+const {createUser,deleteUser,updateUser} = require("../controllers/userController")
 
 router.get("/", (req, res) => {
     res.json({message:"this is user page"})
 })
 
-router.post("/create/:id",createUser)
+router.post("/create/:orgId",createUser)
 
 router.delete("/delete/:id",deleteUser)
+
+router.put("/update/:id",updateUser)
 
 module.exports = router
