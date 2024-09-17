5854af8c4d91a5caa3e28d4fc8cbcbbdacd60c35
zubairmaqsood <149279927+zubairmaqsood@users.noreply.github.com>
2024-09-17 21:10:27 +0500

    completed student create api

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,4 +1,6 @@
 const studentModel = require("../models/studentModel")
+const userModel = require("../models/userModel")
+const organizationModel = require("../models/organizationModel")
 
 module.exports.createStudent = async (req, res) => {
     try{
@@ -16,7 +18,13 @@ module.exports.createStudent = async (req, res) => {
             postalCode
         }
     })
-    res.status(201).json({createdStudent})
+    const userEmail =req.user.email
+    const user = await userModel.findOne({email:userEmail})
+    const organizationId = user.organization.toString()
+    const organization = await organizationModel.findOne({_id:organizationId})
+    organization.students.push(createdStudent._id.toString())
+    await organization.save()
+    res.status(201).json({message:"Student is created"})
     }catch(error){
         if (error.code === 11000 && error.keyValue) {  
             const field = Object.keys(error.keyValue)[0]
